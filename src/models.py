cd18509ae2371563bbcf0b1dae7bbc3b8685cff8
mllrdnl <mllr.dnl@gmail.com>
2021-08-13 01:57:06 +0000

    created people and planet class

--- src/models.py
+++ src/models.py
@@ -8,26 +8,36 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Planet(Base):
+    __tablename__= 'planet'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(250))
+    diameter = Column(String(250))
+    rotation_period = Column(String(250))
+    orbital_period = Column(String(250))
+    gravity = Column(String(250))
+    population = Column(String(250))
+    climate = Column(String(250))
+    terrain = Column(String(250))
+    surface_water = Column(String(250))
+    created = Column(String(250))
+    edited = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class People(Base):
+    __tablename__= 'people'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
+    name = Column(String(250))
+    birth_year = Column(String(250))
+    eye_color = Column(String(250))
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    height = Column(String(250))
+    homeworld_id = Column(Integer, ForeignKey('planet.id'))
+    homeworld = relationship(Planet)
+    mass = Column(String(250))
+    skin_color = Column(String(250))
+    created = Column(String(250))
+    edited = Column(String(250))
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
